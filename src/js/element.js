8c499aa265067a13f1180aa903e172ac43d25bdc
ARCHER04 <62651374+ARCHER04@users.noreply.github.com>
2022-09-11 19:42:00 +0300

    дз 11-12

--- src/js/element.js
+++ src/js/element.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
-    $(".preloader").css("display","none");
-
+    $(".preloader").css("display","none");//прелодер
+    //калькулятор
     function calcul(){
         let sum = parseInt($("#select1 option:selected").val()) + parseInt($("#select2 option:selected").val()) + parseInt($("#select3 option:selected").val());
         let days = parseInt($("#select1 option:selected").attr("days")) + parseInt($("#select2 option:selected").attr("days")) + parseInt($("#select3 option:selected").attr("days"));
@@ -11,7 +11,7 @@ $(document).ready(function(){
         calcul();
     });
     calcul();
-
+    //наши active
     $(window).scroll(() => {
         let scrollDistance = $(window).scrollTop();
 
@@ -28,17 +28,18 @@ $(document).ready(function(){
 
         });
     });
+    //якорь скролл
    $('a[href^="#"').click(function(){
     let valHref=$(this).attr("href");
     $('html, body').animate({scrollTop:$(valHref).offset().top-68+ "px"})
    });
-   
+   //модальное окно
    window.addEventListener('load', function() {
     setTimeout(function() { 
         $('.modal').modal('show');
     }, 10000);
     });
-
+    //бегающее цифры
    var show = true;
    var countbox = ".benefits__inner";
    $(window).on("scroll load resize", function () {
@@ -48,17 +49,33 @@ $(document).ready(function(){
        var w_height = $(window).height(); // Высота окна браузера
        var d_height = $(document).height(); // Высота всего документа
        var e_height = $(countbox).outerHeight(); // Полная высота блока со счетчиками
-       if (w_top + 1000>= e_top || w_height + w_top == d_height || e_height + e_top < w_height) {
-           $('#number').css('opacity', '1');
-           $('#number').spincrement({
+       if (w_top + 500>= e_top || w_height + w_top == d_height || e_height + e_top < w_height) {
+           $('.number').css('opacity', '1');
+           $('.number').spincrement({
             thousandSeparator: "",
             duration: 1200
-            });
+        });
             
         
            show = false;
        }
    });
-
-
+   //планое появление
+   let options={threshold:[0.5]};
+   let observer=new IntersectionObserver(onEntry,options);
+   let element=$('.activ-animate');
+   element.each((i,el)=>{
+        observer.observe(el);
+   });
+   function onEntry(entry){
+    entry.forEach(change => {
+        if(change.isInteresting){
+            change.target.classList.add('activ_show');
+        }
+    });
+   }
+   //magnificpopup
+   $(document).ready(function() {
+    $('.image-link').magnificPopup({type:'image'});
+  });
 });
